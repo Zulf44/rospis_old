44b7c7f85261f71b1fffd36bc44d85e6a5486c93
Zulf44 <73906005+Zulf44@users.noreply.github.com>
2022-01-17 20:20:41 +0300

    beta_0

--- js/script.js
+++ js/script.js
@@ -5,28 +5,28 @@ const App = {
          nambers: [],
          lessons: [
             {
-               lessonStart: new Date(2022, 0, 13, 11, 20),
+               lessonStart: new Date(2022, 0, 19, 11, 20),
                lessonName: "Организационное собрание",
                classroom: "А",
                teacher: "Дирекция ИЦТЭ",
                lessonType: "Лекция"
             },
             {
-               lessonStart: new Date(2022, 0, 14, 13, 20),
+               lessonStart: new Date(2022, 0, 19, 13, 20),
                lessonName: "Теория систем и системный анализ",
                classroom: "Д-418",
                teacher: "доц. Андреев В.В.",
                lessonType: "Лекция"
             },
             {
-               lessonStart: new Date(2022, 0, 14, 15, 00),
+               lessonStart: new Date(2022, 0, 19, 15, 00),
                lessonName: "Пакеты прикладных программ",
                classroom: "В-619",
                teacher: "ст.пр. Эшелиоглу Р.И.",
                lessonType: "Лекция"
             },
             {
-               lessonStart: new Date(2022, 0, 14, 8, 00),
+               lessonStart: new Date(2022, 0, 20, 8, 00),
                lessonName: "Объектно-ориентированное программирование на языке C#",
                classroom: "В-619",
                teacher: "доц. Шустова К.П.",
@@ -40,7 +40,7 @@ const App = {
                lessonType: "Лекция"
             },
             {
-               lessonStart: new Date(2022, 0, 14, 11, 20),
+               lessonStart: new Date(2022, 0, 20, 11, 20),
                lessonName: "Теория систем и системный анализ",
                classroom: "Д-420",
                teacher: "доц. Андреев В.В.",
@@ -53,11 +53,265 @@ const App = {
                teacher: "доц. Андреев В.В.",
                lessonType: "Лаб."
             },
+            {
+               lessonStart: new Date(2022, 0, 21, 9, 40),
+               lessonName: "Теория систем и системный анализ",
+               classroom: "Е-100(8)",
+               teacher: "доц. Андреев В.В.",
+               lessonType: "Лаб."
+            },
+            {
+               lessonStart: new Date(2022, 0, 21, 11, 20),
+               lessonName: "Базы данных",
+               classroom: "Д-624",
+               teacher: "доц. Николаева С.Г.",
+               lessonType: "Практика"
+            },
+            {
+               lessonStart: new Date(2022, 0, 21, 13, 20),
+               lessonName: "Объектно-ориентированное программирование на языке C#",
+               classroom: "Г-325",
+               teacher: "Низямова А.Р.",
+               lessonType: "Практика"
+            },
+            {
+               lessonStart: new Date(2022, 0, 21, 15, 00),
+               lessonName: "Статистика",
+               classroom: "Д-508",
+               teacher: "доц. Будникова И.К",
+               lessonType: "Лекция"
+            },
+            {
+               lessonStart: new Date(2022, 0, 22, 8, 00),
+               lessonName: "Базы данных ",
+               classroom: "А-321",
+               teacher: "доц. Николаева С.Г.",
+               lessonType: "Лаб."
+            },
+            {
+               lessonStart: new Date(2022, 0, 22, 9, 40),
+               lessonName: "Базы данных ",
+               classroom: "А-321",
+               teacher: "доц. Николаева С.Г.",
+               lessonType: "Лаб."
+            },
+            {
+               lessonStart: new Date(2022, 0, 22, 11, 20),
+               lessonName: "Статистика",
+               classroom: "Е-100(4)",
+               teacher: "доц. Будникова И.К.",
+               lessonType: "Лаб."
+            },
+            {
+               lessonStart: new Date(2022, 0, 22, 13, 20),
+               lessonName: "Статистика",
+               classroom: "Е-100(4)",
+               teacher: "доц. Будникова И.К.",
+               lessonType: "Лаб."
+            },
+            {
+               lessonStart: new Date(2022, 0, 24, 8, 00),
+               lessonName: "Пакеты прикладных программ",
+               classroom: "Г-323",
+               teacher: "доц. Салтанаева Е.А.",
+               lessonType: "Лаб."
+            },
+            {
+               lessonStart: new Date(2022, 0, 24, 9, 40),
+               lessonName: "Пакеты прикладных программ",
+               classroom: "Г-323",
+               teacher: "доц. Салтанаева Е.А.",
+               lessonType: "Лаб."
+            },
+            {
+               lessonStart: new Date(2022, 0, 24, 11, 20),
+               lessonName: "Теория систем и системный анализ",
+               classroom: "А-413",
+               teacher: "доц. Андреев В.В.",
+               lessonType: "Практика"
+            },
+            {
+               lessonStart: new Date(2022, 0, 24, 13, 20),
+               lessonName: "Базы данных",
+               classroom: "Д-622",
+               teacher: "доц. Николаева С.Г.",
+               lessonType: "Лаб."
+            },
+            {
+               lessonStart: new Date(2022, 0, 25, 8, 0),
+               lessonName: "Теория систем и системный анализ",
+               classroom: "Е-100(7)",
+               teacher: "доц. Андреев В.В.",
+               lessonType: "Лаб."
+            },
+            {
+               lessonStart: new Date(2022, 0, 25, 9, 40),
+               lessonName: "Теория систем и системный анализ",
+               classroom: "Е-100(7)",
+               teacher: "доц. Андреев В.В.",
+               lessonType: "Лаб."
+            },
+            {
+               lessonStart: new Date(2022, 0, 25, 11, 20),
+               lessonName: "Объектно-ориентированное программирование на языке C#",
+               classroom: "Г-218",
+               teacher: "Низямова А.Р.",
+               lessonType: "Практика"
+            },
+            {
+               lessonStart: new Date(2022, 0, 25, 13, 20),
+               lessonName: "Настройка и администрирование компьютерных сетей",
+               classroom: "Г-324",
+               teacher: "доц. Ситников С.Ю.",
+               lessonType: "Лаб."
+            },
+            {
+               lessonStart: new Date(2022, 0, 25, 15, 00),
+               lessonName: "Настройка и администрирование компьютерных сетей",
+               classroom: "Г-324",
+               teacher: "доц. Ситников С.Ю.",
+               lessonType: "Лаб."
+            },
+            {
+               lessonStart: new Date(2022, 0, 26, 8, 00),
+               lessonName: "Теория систем и системный анализ",
+               classroom: "Е-100(7)",
+               teacher: "доц. Андреев В.В.",
+               lessonType: "Лекция"
+            },
+            {
+               lessonStart: new Date(2022, 0, 26, 9, 40),
+               lessonName: "доц. Андреев В.В.",
+               classroom: "В-621",
+               teacher: "Низямова А.Р.",
+               lessonType: "Практика"
+            },
+            {
+               lessonStart: new Date(2022, 0, 26, 11, 20),
+               lessonName: "преп. Низямова А.Р.",
+               classroom: "Д-427",
+               teacher: "доц. Николаева С.Г.",
+               lessonType: "Практика"
+            },
+            {
+               lessonStart: new Date(2022, 0, 27, 13, 20),
+               lessonName: "Объектно-ориентированное программирование на языке C#",
+               classroom: "Д-427",
+               teacher: "ст.пр. Коростелева Д.М.",
+               lessonType: "Лаб."
+            },
+            {
+               lessonStart: new Date(2022, 0, 27, 15, 00),
+               lessonName: "Объектно-ориентированное программирование на языке C#",
+               classroom: "Д-427",
+               teacher: "ст.пр. Коростелева Д.М.",
+               lessonType: "Лаб."
+            },
+            {
+               lessonStart: new Date(2022, 0, 27, 16, 40),
+               lessonName: "Информационные системы",
+               classroom: "Д-508",
+               teacher: "доц. Сибаева Г.Р.",
+               lessonType: "Лекция(?)"
+            },
+            {
+               lessonStart: new Date(2022, 0, 28, 9, 40),
+               lessonName: "Настройка и администрирование компьютерных сетей",
+               classroom: "В-621",
+               teacher: "доц. Ситников С.Ю.",
+               lessonType: "Зачет"
+            },
+            {
+               lessonStart: new Date(2022, 0, 28, 11, 20),
+               lessonName: "Настройка и администрирование компьютерных сетей",
+               classroom: "В-621",
+               teacher: "доц. Ситников С.Ю.",
+               lessonType: "Зачет"
+            },
+            {
+               lessonStart: new Date(2022, 0, 29, 9, 40),
+               lessonName: "Базы данных",
+               classroom: "В-621",
+               teacher: "доц. Николаева С.Г",
+               lessonType: "Экзамен"
+            },
+            {
+               lessonStart: new Date(2022, 0, 29, 11, 20),
+               lessonName: "Базы данных",
+               classroom: "В-621",
+               teacher: "доц. Николаева С.Г",
+               lessonType: "Экзамен"
+            },
+            {
+               lessonStart: new Date(2022, 0, 29, 13, 20),
+               lessonName: "Базы данных",
+               classroom: "В-621",
+               teacher: "доц. НиколаевЭкзамен",
+               lessonType: "Экзамен"
+            },
+            {
+               lessonStart: new Date(2022, 1, 1, 8, 00),
+               lessonName: "Пакеты прикладных программ",
+               classroom: "В-621",
+               teacher: "Эшелиоглу Р.И.",
+               lessonType: "Экзамен"
+            },
+            {
+               lessonStart: new Date(2022, 1, 2, 8, 00),
+               lessonName: "Статистика",
+               classroom: "В-621",
+               teacher: "доц. Будникова И.К.",
+               lessonType: "Зачет"
+            },
+            {
+               lessonStart: new Date(2022, 1, 4, 8, 00),
+               lessonName: "Объектно-ориентированное программирование на языке C#",
+               classroom: "В-621",
+               teacher: "доц. Шустова К.П.",
+               lessonType: "Экзамен"
+            },
+            {
+               lessonStart: new Date(2022, 1, 7, 8, 00),
+               lessonName: "Теория систем и системный анализ",
+               classroom: "В-621",
+               teacher: "доц. Андреев В.В.",
+               lessonType: "Экзамен"
+            },
+            {
+               lessonStart: new Date(2022, 1, 9, 11, 20),
+               lessonName: "WEB-программирование",
+               classroom: "В-621",
+               teacher: "ст.пр. Галиев Р.И.",
+               lessonType: "Лекция"
+            },
+            {
+               lessonStart: new Date(2022, 1, 9, 13, 20),
+               lessonName: "Операционные системы",
+               classroom: "В-621",
+               teacher: "доц. Киселев Н.С.",
+               lessonType: "Лекция"
+            },
+            {
+               lessonStart: new Date(2022, 1, 9, 15, 00),
+               lessonName: "Цифровая экономика",
+               classroom: "В-621",
+               teacher: "доц. Каляшина А.В.",
+               lessonType: "Лекция"
+            },
+            {
+               lessonStart: new Date(2022, 1, 9, 18, 20),
+               lessonName: "Информационная безопасность",
+               classroom: "В-621",
+               teacher: "доц. Исмагилов И.Р.",
+               lessonType: "Лекция"
+            },
          ],
          dayOfWeek: "",
          dateToday: "",
          toDay: new Date,
          lessonToday: [],
+         calendar: [],
+         toDayStatic: new Date
 
 
 
@@ -113,14 +367,81 @@ const App = {
          this.toDay.setTime(this.toDay.getTime() + 86400000);
          this.getdayOfWeek();
          this.getLesonToday(this.toDay);
+         this.calendarInit(this.toDay)
       },
       reduceDate() {
          this.toDay.setTime(this.toDay.getTime() - 86400000);
          this.getdayOfWeek();
          this.getLesonToday(this.toDay);
+         this.calendarInit(this.toDay)
+      },
+      calendarInit(today) {
+         this.calendar = []
+         // По новой
+
+         // Получаем количество дней в текущем месяце 
+         today.getDaysInMounth = function () {
+
+            let date1 = new Date(this.getFullYear(), this.getMonth(), 1);
+            let date2 = new Date(this.getFullYear(), (this.getMonth() + 1), 1);
+            let date3 = Math.round((date2 - date1) / 1000 / 3600 / 24);
+            return date3;
+         }
+         console.log(today.getDaysInMounth())
+
+         // Получаем день недели первого числа месяца
+
+         today.getDayWorld = function () {
+            todayClone = new Date(this.getFullYear(), this.getMonth())
+            switch (todayClone.getDay()) {
+               case 0:
+                  return 6;
+                  break;
+               case 1:
+                  return 0;
+                  break;
+               case 2:
+                  return 1;
+                  break;
+               case 3:
+                  return 2;
+                  break;
+               case 4:
+                  return 3;
+                  break;
+               case 5:
+                  return 4;
+                  break;
+               case 6:
+                  return 5;
+                  break;
+
+            }
+         }
+
+         console.log(today.getDayWorld())
+
+         // добавляем нули в перед
+         for (i = 1; i < today.getDayWorld() + 1; i++) {
+            this.calendar.push(0)
+         }
+
+         // добовляем числа месяца в массив
+         for (i = 1; i <= today.getDaysInMounth(); i++) {
+            this.calendar.push(i)
+         }
+
+      },
+      switchDateFromCalendar(day) {
+         this.toDay.setDate(day)
+         this.calendarInit(this.toDay)
+         this.getdayOfWeek()
+         this.getLesonToday(this.toDay)
       }
+
    },
    mounted() {
+      this.calendarInit(this.toDay)
       this.getdayOfWeek()
       this.getLesonToday(this.toDay)
    }
@@ -230,49 +551,6 @@ Vue.createApp(App).mount("#app")
 //    console.log(w)
 // }
 
-// function Calendar2(id, year, month) {
-//    var Dlast = new Date(year, month + 1, 0).getDate(),
-//       D = new Date(year, month, Dlast),
-//       DNlast = new Date(D.getFullYear(), D.getMonth(), Dlast).getDay(),
-//       DNfirst = new Date(D.getFullYear(), D.getMonth(), 1).getDay(),
-//       calendar = '<tr>',
-//       month = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
-//    if (DNfirst != 0) {
-//       for (var i = 1; i < DNfirst; i++) calendar += '<td>';
-//    } else {
-//       for (var i = 0; i < 6; i++) calendar += '<td>';
-//    }
-//    for (var i = 1; i <= Dlast; i++) {
-//       if (i == new Date().getDate() && D.getFullYear() == new Date().getFullYear() && D.getMonth() == new Date().getMonth()) {
-//          calendar += '<td class="today">' + i;
-//       } else {
-//          calendar += '<td>' + i;
-//       }
-//       if (new Date(D.getFullYear(), D.getMonth(), i).getDay() == 0) {
-//          calendar += '<tr>';
-//       }
-//    }
-//    for (var i = DNlast; i < 7; i++) calendar += '<td>&nbsp;';
-//    document.querySelector('#' + id + ' tbody').innerHTML = calendar;
-//    document.querySelector('#' + id + ' thead td:nth-child(2)').innerHTML = month[D.getMonth()] + ' ' + D.getFullYear();
-//    document.querySelector('#' + id + ' thead td:nth-child(2)').dataset.month = D.getMonth();
-//    document.querySelector('#' + id + ' thead td:nth-child(2)').dataset.year = D.getFullYear();
-//    if (document.querySelectorAll('#' + id + ' tbody tr').length < 6) {  // чтобы при перелистывании месяцев не "подпрыгивала" вся страница, добавляется ряд пустых клеток. Итог: всегда 6 строк для цифр
-//       document.querySelector('#' + id + ' tbody').innerHTML += '<tr><td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;';
-//    }
-// }
-// Calendar2("calendar2", new Date().getFullYear(), new Date().getMonth());
-// // переключатель минус месяц
-// document.querySelector('#calendar2 thead tr:nth-child(1) td:nth-child(1)').onclick = function () {
-//    Calendar2("calendar2", document.querySelector('#calendar2 thead td:nth-child(2)').dataset.year, parseFloat(document.querySelector('#calendar2 thead td:nth-child(2)').dataset.month) - 1);
-// }
-// // переключатель плюс месяц
-// document.querySelector('#calendar2 thead tr:nth-child(1) td:nth-child(3)').onclick = function () {
-//    Calendar2("calendar2", document.querySelector('#calendar2 thead td:nth-child(2)').dataset.year, parseFloat(document.querySelector('#calendar2 thead td:nth-child(2)').dataset.month) + 1);
-// }
-let today = new Date;
 
-console.log(today);
-today.setTime(today.getTime() + 86400000)
 
-console.log(today);
+
